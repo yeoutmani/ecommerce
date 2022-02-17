bbc61b69ed73ce915cf11f43bd96efd3eb8bb529
yeoutmani <yeoutmani@somone.fr>
2022-02-17 10:55:38 +0100

    shop & home & sign in pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+const HatsPage = (props) => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shop" element={<ShopPage />} />
+        <Route path="shop/hats" element={<HatsPage />} />
+        <Route path="sign" element={<SignInAndSignUpPage />} />
+
+      </Routes>
     </div>
   );
 }
