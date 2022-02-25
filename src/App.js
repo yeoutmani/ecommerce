5893f87685981a65b9bd1650f40ad767164ce207
mbenchrifa <mbenchrifa@somone.fr>
2022-02-25 13:41:18 +0100

    sign up with email and password (firebase)

--- src/App.js
+++ src/App.js
@@ -1,57 +1,40 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, db } from './firebase/firebase.utils';
-import { collection, getDoc, doc } from "firebase/firestore";
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
-  createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
 
-    const userRef = collection(db, 'users');
-    //const snapShot = await getDocs(userRef);
-    console.log('userRef', userRef);
-
-    const userRefs = doc(db, 'users/1551515fdfe');
-    const docSnap = await getDoc(userRefs);
-    console.log("docSnap", docSnap);
-    console.log("Document data:", docSnap.data());
-
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-
-    /*const citiesCol = collection(db, 'users');
-    const citySnapshot = await getDocs('citiesCol/128feefe');
-    const cityList = citySnapshot.docs.map(doc => ({...doc.data(), id:doc.id}));
-
-    console.log('citiesCol',citiesCol);
-    console.log('citySnapshot',citySnapshot);
-    console.log('cityList',cityList);*/
-
-  }
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
   componentDidMount() {
-    auth.onAuthStateChanged(async user => {
-      this.setState({ currentUser: user });
-      console.log("user", user);
-      this.createUserProfileDocument(user);
-
-    })
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        console.log('Document data:', userRef.data());
+        this.setState({
+          currentUser: {
+            id: userRef.id,
+            ...userRef.data(),
+          },
+        });
+        console.log(this.state);
+      }
+      /* this.setState({ currentUser: user });
+      console.log('user', user);
+      this.createUserProfileDocument(user);*/
+      this.setState({ currentUser: userAuth });
+    });
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -64,7 +47,6 @@ class App extends React.Component {
           <Route path="/" element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
           <Route path="sign" element={<SignInAndSignUpPage />} />
-
         </Routes>
       </div>
     );
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
+import { collection, getDoc, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDbaurjyyDS-1vw43r4yp4cnC1KBSVGxk8",
-  authDomain: "crwn-db-d7457.firebaseapp.com",
-  projectId: "crwn-db-d7457",
-  storageBucket: "crwn-db-d7457.appspot.com",
-  messagingSenderId: "171465506134",
-  appId: "1:171465506134:web:1b6ccd5413359f929a2af7",
-  measurementId: "G-MMTCXJDQH1"
+  apiKey: 'AIzaSyDbaurjyyDS-1vw43r4yp4cnC1KBSVGxk8',
+  authDomain: 'crwn-db-d7457.firebaseapp.com',
+  projectId: 'crwn-db-d7457',
+  storageBucket: 'crwn-db-d7457.appspot.com',
+  messagingSenderId: '171465506134',
+  appId: '1:171465506134:web:1b6ccd5413359f929a2af7',
+  measurementId: 'G-MMTCXJDQH1',
 };
 var app = initializeApp(firebaseConfig);
 
@@ -19,3 +20,30 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export default app;
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const docRef = doc(db, 'users', userAuth.uid);
+  const snapshot = await getDoc(docRef);
+  console.log('snapshot', snapshot.exists());
+
+  const collectionRef = collection(db, 'users');
+
+  if (!snapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+    try {
+      console.log('yes');
+      await setDoc(doc(collectionRef, userAuth.uid), {
+        displayName,
+        email,
+        createAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error);
+    }
+  }
+  return snapshot;
+};
