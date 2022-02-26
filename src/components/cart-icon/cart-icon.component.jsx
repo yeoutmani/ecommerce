441d3f0541aa33cddddb3dcc74c88a6fbf5fbd61
yeotmani <ysn.outmani@gmail.com>
2022-02-26 20:18:03 +0100

    updation app woth reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,27 @@ import { ReactComponent as ShopingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShopingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 
 const mapDispatchToPros = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
+/*
+La méthode reduce() applique une fonction qui est un « accumulateur » 
+et qui traite chaque valeur d'une liste (de la gauche vers la droite) 
+afin de la réduire à une seule valeur.
+*/
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce(
+        (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+        0
+      )
+});
 
-export default connect(null,mapDispatchToPros)(CartIcon);
+
+export default connect(mapStateToProps, mapDispatchToPros)(CartIcon);
